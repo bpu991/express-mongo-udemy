a44395e995447e30e0309820bed9e78f20472994
Ben Pu <bpu991@gmail.com>
2021-02-22 18:48:20 -0500

    mongo db set up

---
+++ server/services/passport.js
@@ -0,0 +1,33 @@
+const passport = require('passport');
+const GoogleStrategy = require('passport-google-oauth20').Strategy;
+const mongoose = require('mongoose');
+const keys = require('../config/keys');
+
+const User = mongoose.model('users'); // fetches model class from models dir
+
+passport.serializeUser((user, done) => {
+    done(null, user.id);
+});
+
+passport.use(
+    new GoogleStrategy(
+        {
+            clientID: keys.googleClientID,
+            clientSecret: keys.googleClientSecret,
+            callbackURL: '/auth/google/callback'
+        },
+        (accessToken, refreshToken, profile, done) => {
+            User.findOne({ googleId: profile.id })
+                .then((existingUser) => {
+                    if (existingUser) { // means we already have a record with this id
+                        done(null, existingUser);
+                    } else { // creates a new user with the profile id
+                        new User({ googleId: profile.id })
+                        .save() //.save() saves data to the database
+                        .then(user => done(null, user));
+                    }
+                })
+            
+        }
+    )
+);
