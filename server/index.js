17a5fdcc53b702d3bece344b12db3e5161ee978d
Ben Pu <bpu991@gmail.com>
2021-02-23 12:17:41 -0500

    google auth working

--- server/index.js
+++ server/index.js
@@ -1,5 +1,7 @@
 const express = require('express');
 const mongoose = require('mongoose');
+const cookieSession = require('cookie-session');
+const passport = require('passport');
 const keys = require('./config/keys');
 
 require('./models/User.js');
@@ -7,6 +9,16 @@ require('./services/passport.js');
 
 mongoose.connect(keys.mongoURI)
 const app = express();
+
+app.use(
+    cookieSession({
+        maxAge: 30 * 24 * 60 * 60 * 1000, // how long this cookie will be used before expiration (30 days)
+        keys: [keys.cookieKey] // key will be used to encrypt cookie
+    })
+);
+
+app.use(passport.initialize());
+app.use(passport.session());
 
 require('./routes/authRoutes')(app);
 
